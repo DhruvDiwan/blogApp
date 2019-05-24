0216f18b1d38ce4326d9f6a9f013866b9fd6e55c
DhruvDiwan <dhruv18031@iiitd.ac.in>
2019-05-24 22:38:28 +0530

    added blog_edit url to blog/urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BlogListView , BlogDetailView , BlogCreateView
+from .views import BlogListView , BlogDetailView , BlogCreateView , BlogUpdateView
 
 urlpatterns = [
+	path('blog/<int:pk>/edit/' , BlogUpdateView.as_view() , name='blog_edit'),
 	path('blog/new/' , BlogCreateView.as_view() , name='blog_new'),
 	path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
 	path('' , BlogListView.as_view() , name='home'),
