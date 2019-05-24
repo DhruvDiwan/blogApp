7fc3523a3cb47c2e50e9f8f2ab15e736234a94db
DhruvDiwan <dhruv18031@iiitd.ac.in>
2019-05-24 21:46:35 +0530

    Added BlogCreateView to views.py

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from .models import Blog
 from django.views.generic import ListView , DetailView
-
+from django.views.generic.edit import CreateView
 
 class BlogListView(ListView):
 	model = Blog
@@ -9,3 +9,8 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
 	model = Blog
 	template_name = 'blog_detail.html'
+
+class BlogCreateView(CreateView):
+	model = Blog
+	template_name = 'blog_new.html'
+	fields = '__all__'
